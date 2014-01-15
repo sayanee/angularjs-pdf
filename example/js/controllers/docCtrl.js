1fb0737bf97af686197824d18ebe094ced3696fc
Sayanee <sayanee@gmail.com>
2014-01-15 19:51:46 +0800

    removed not require template scope variable

--- example/js/controllers/docCtrl.js
+++ example/js/controllers/docCtrl.js
@@ -2,7 +2,6 @@ app.controller('DocCtrl', function($scope) {
 
   $scope.pdfName = 'Relativity: The Special and General Theory by Albert Einstein';
   $scope.pdfUrl = '/pdf/relativity.pdf';
-  $scope.template = 'partials/viewer.html';
   $scope.scroll = 0;
 
 });
