929d4a241199e944bedfda4fb4deaafc5833e599
Sayanee <sayanee@gmail.com>
2014-01-15 16:40:26 +0800

    example

---
+++ example/js/app.js
@@ -0,0 +1,2 @@
+var app = angular.module('App', []);
+
---
+++ example/js/controllers/docCtrl.js
@@ -0,0 +1,8 @@
+app.controller('DocCtrl', function($scope) {
+
+  $scope.pdfName = 'Relativity: The Special and General Theory by Albert Einstein';
+  $scope.pdfUrl = '/pdf/relativity.pdf';
+  $scope.template = 'partials/viewer.html';
+  $scope.scroll = 0;
+
+});
