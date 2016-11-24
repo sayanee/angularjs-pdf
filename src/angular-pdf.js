b56f318ea703e3a39ae5d80d8a5be6d682be3239
Denny Biasiolli <denny.biasiolli@gmail.com>
2016-11-24 22:13:26 +0100

    fix: removing warning in getDocument call
    Warning message was:
    Deprecated API usage: getDocument is called with pdfDataRangeTransport, passwordCallback or progressCallback

--- src/angular-pdf.js
+++ src/angular-pdf.js
@@ -165,7 +165,8 @@
           }
 
           if (url && url.length) {
-            pdfLoaderTask = PDFJS.getDocument(params, null, null, scope.onProgress);
+            pdfLoaderTask = PDFJS.getDocument(params);
+            pdfLoaderTask.onProgress = scope.onProgress;
             pdfLoaderTask.then(
                 function(_pdfDoc) {
                   if (typeof scope.onLoad === 'function') {
